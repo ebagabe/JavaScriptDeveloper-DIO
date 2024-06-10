803ff22460ba8845c706df57c36f9f8198cdc463
ebagabe <gabriel.ebagabe@gmail.com>
2024-06-10 09:20:17 -0400

    verifyAge

--- functions/exercise01.js
+++ functions/exercise01.js
@@ -3,3 +3,11 @@ function writeMyName(name) {
 };
 
 writeMyName("Gabriel");
+
+function verifyAge(age) {
+    if (age >= 18) {
+        console.log('Of legal age')
+    } else {
+        console.log('Underage');
+    }
+}
