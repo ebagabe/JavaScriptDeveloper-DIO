9ebff935b52cc0cb204fefabdfe2d77eb2c1a238
ebagabe <gabriel.ebagabe@gmail.com>
2024-06-10 14:05:04 -0400

    Creating a function about the object

--- Objects/class.js
+++ Objects/class.js
@@ -15,8 +15,19 @@ class People {
     }
 }
 
-const gabe = new People('Gabe', 22);
-const joy = new People('Joy', 18);
+function comparePeoples(p1, p2) {
+    if (p1.age > p2.age) {
+        return `The ${p1.name} is older than ${p2.name}`
+    }
 
-gabe.description();
-joy.description();
+    if (p2.age > p1.age) {
+        return `The ${p2.name} is older than ${p1.name}`
+    }
+
+    return `Both have the same age`
+}
+
+const people1 = new People("Gabe", 22);
+const people2 = new People("Joy", 18);
+
+console.log(comparePeoples(people1, people2));
